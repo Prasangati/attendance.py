8ea6b96ee8d4440536ed18b16a7be3768ab62133
Prasangati <53667641+Prasangati@users.noreply.github.com>
2023-04-19 18:05:13 -0400

    check
    check

--- attendanceautomation.py
+++ attendanceautomation.py
@@ -4,9 +4,7 @@ import time
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-import google.auth.exceptions
+
 
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
@@ -17,7 +15,7 @@ driver = webdriver.Chrome('drivers/chromedriver.exe', chrome_options=chrome_opti
 # Navigate to the Google sign-in page
 url = 'https://docs.google.com/forms/d/e/1FAIpQLSdgh6hUBkUe99r3BoNEPnDDxo5kMUjl-PoRenbOz_Y-xAVOsA/viewform'
 email = "prasangatiwari@nypl.org"
-password = ""
+password = "Ibrabest10-"
 driver.get(url)
 driver.implicitly_wait(10)  #Wait until the program loads
 
@@ -33,7 +31,11 @@ time.sleep(30)
 
 grade_id = {              # identifies the id based on the grade  
   "Pre-K": "i116",
+  "pre-K": "i116",
   "Kindergarten": "i119",
+  "kindergarten": "i119",
+  "K": "i119",
+  "k": "i119",
   "1": "i122",
   "2": "i125",
   "3": "i128",
@@ -44,12 +46,6 @@ grade_id = {              # identifies the id based on the grade
   "8": "i143"
 }
 
-clearform = driver.find_element(By.XPATH, '/html/body/div/div[2]/form/div[2]/div/div[3]/div[1]/div[2]/div/span/span')
-clearform.click()
-
-time.sleep(7)
-
-
 #A CSV file is open and it reads data from the file
 with open('data.csv', 'r') as file:
     attendance = csv.reader(file)
@@ -57,11 +53,12 @@ with open('data.csv', 'r') as file:
     next(attendance)
     for row in attendance:
         #ignores the name and the branch because that is unnecessary
-        name,date, school_name, grade, unique, math, ela, science, social_studies, other  = row[0:]
+        check = driver.find_element(By.CLASS_NAME, 'd7L4fc bJNwt  FXLARc aomaEc ECvBRb')
+        check.click()
+        name,date, school_name, grade, unique, math, ela, science, social_studies, other  = row[:]
 
         #branch is already chosen
-        element = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div/span/div/div[21]/label/div/div[2]/div/span')
-        element.click()
+       
         month, day = date.split("/") #there is no input for year
         month = month.zfill(2)
         day = day.zfill(2)
@@ -81,23 +78,23 @@ with open('data.csv', 'r') as file:
         grade_options.click()
 
         unique_ = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/form/div[2]/div/div[2]/div[6]/div/div/div[2]/div[1]/div/span/div/div/label/div/div[1]/div/div[3]/div') #done
-        if unique == 'Yes' or unique == 'yes':
+        if (unique != "") and (unique[0] == 'Y' or unique[0] == 'y'):
             unique_.click()
 
         math_choice = driver.find_element(By.XPATH, '/html/body/div/div[2]/form/div[2]/div/div[2]/div[7]/div/div/div[2]/div[1]/div/span/div/div/label/div/div[1]/div/div[3]/div') #done
-        if math == 'Yes' or math == 'yes':
+        if (math != "") and (math[0] == 'Y' or math[0] == 'y'):
             math_choice.click()
 
         ela_choice = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/form/div[2]/div/div[2]/div[8]/div/div/div[2]/div[1]/div/span/div/div/label')  #done
-        if ela == 'Yes' or ela == 'yes':
+        if (ela != "") and (ela[0] == 'Y' or ela[0] == 'y'):
             ela_choice.click()
 
         science_choice = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/form/div[2]/div/div[2]/div[9]/div/div/div[2]/div[1]/div/span/div/div/label/div/div[1]/div/div[3]/div')
-        if science == 'Yes' or science == 'yes':
+        if (science != "") and (science[0] == 'Y' or science[0] == 'y'):
             science_choice.click()
 
         social_studies_choice = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/form/div[2]/div/div[2]/div[10]/div/div/div[2]/div[1]/div/span/div/div')
-        if social_studies == 'Yes' or social_studies == 'yes':
+        if (social_studies != "") and (social_studies[0] == 'Y' or social_studies[0] == 'y'):
             social_studies_choice.click()
 
         other_ = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/form/div[2]/div/div[2]/div[11]/div/div/div[2]/div[1]/div/span/div/div')
@@ -106,5 +103,10 @@ with open('data.csv', 'r') as file:
 
         other_for_name = driver.find_element(By.XPATH, '/html/body/div/div[2]/form/div[2]/div/div[2]/div[12]/div/div/div[2]/div/div[1]/div[2]/textarea')
         other_for_name.send_keys(name)
-        
+
+        submit = driver.find_element(By.CLASS_NAME, 'NPEfkd RveJvd snByac')
+        submit.click()
+
+        another_form = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[1]/div/div[4]/a[2]')
+        another_form.click()
 
